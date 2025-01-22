47a1d692bcf172c3781f5ea21cfc6d79d13bab6b
nicholllllssssss <161303273+nicholllllssssss@users.noreply.github.com>
2025-01-22 13:30:33 +1030

    Update KC ACTIVITY.php

--- KC ACTIVITY.php
+++ KC ACTIVITY.php
@@ -1,13 +1,13 @@
 <?php
 class Product {
-    // Define properties
+   
     private $name;
     private $price;
     private $description;
     private $category;
     private $stock;
 
-    // Method to set product details
+   
     public function setProduct($name, $price, $description, $category, $stock) {
         $this->name = $name;
         $this->price = $price;
@@ -16,7 +16,7 @@ class Product {
         $this->stock = $stock;
     }
 
-    // Method to get product information
+    
     public function getProductInfo() {
         return "Product Name: $this->name\n" .
                "Price: $this->price\n" .
@@ -26,7 +26,7 @@ class Product {
     }
 }
 
-// Create an object of Product
+
 $product = new Product();
 $product->setProduct("towel", 4$, "best towel", "bath sheets", 20);
 echo $product->getProductInfo();
